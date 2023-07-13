313a5ce3f32c7890e965ef86288222ea20974a0d
Artem303 <125798638+Artem303@users.noreply.github.com>
2023-07-13 13:18:17 +0300

    finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listGallery = document.querySelector('.gallery');
+
+const listGalleryImg = createItems(galleryItems);
+
+function createItems(galleryItems) {
+  return galleryItems
+    .map(
+      item =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+    )
+    .join('');
+}
+
+listGallery.innerHTML = listGalleryImg;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
